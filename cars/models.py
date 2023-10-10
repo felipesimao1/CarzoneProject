ad00d0c87d80824801b74755163f84aae1f7250d
felipesimao1 <felipe.simao1@gmail.com>
2023-10-10 15:34:49 +0100

    finalizing home page

--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from multiselectfield import MultiSelectField
 
 
 # Create your models here.
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Team
+from cars.models import Car
 
 # Create your views here.
 
 def home(request):
     teams = Team.objects.all() # get all the objects from the Team model
+    is_published = Car.objects.order_by('-created_date').filter(is_published=True) # get all the objects from the Car model ordered by the created_date and filtered by is_featured
     data = {
         'teams': teams,
+        'is_published': is_published,
     }
     return render(request, 'pages/home.html', data)
 
