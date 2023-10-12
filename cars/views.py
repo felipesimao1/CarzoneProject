f6893fc3df9f23089484f521fd98bc20fad40402
felipesimao1 <felipe.simao1@gmail.com>
2023-10-12 16:25:58 +0100

    car page

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Car
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def cars(request):
-    return render(request, 'cars/cars.html') # this is the home page of cars app inside templates/cars/cars.html
+    cars = Car.objects.order_by('-created_date') # this is the car model inside models.py
+    paginator = Paginator(cars, 4) # Show 4 cars per page.
+    page = request.GET.get('page') # this is the page number
+    page_cars = paginator.get_page(page) # this is the page number
+    data = {
+        'cars': page_cars # this is the cars variable
+
+    }
+    return render(request, 'cars/cars.html', data) # this is the home page of cars app inside templates/cars/cars.html
+
+def car_details(request, id):
+    single_car = get_object_or_404(Car, pk=id)
+
+    data = {
+        'single_car': single_car
+    }
+    return render(request, 'cars/car_details.html', data) # this is the car detail page inside templates/cars/car_details.html
