f9cd92e6ea24f831a2be329a1fbc1709ca5fa873
felipesimao1 <felipe.simao1@gmail.com>
2023-10-26 15:07:44 +0100

    deploy

--- carzone/settings.py
+++ carzone/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'Pillow',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
 
